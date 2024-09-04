131e3a2bcca5fc4c0368ad6c8b2d6e7364764742
k1m190r <k1m190r@outlook.com>
2024-09-04 23:28:11 +0200

    add fig 2.5

--- 99/main.c
+++ 99/main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main() {
-  int niter = 1'000;
+  int niter = 10'000;
   srand((unsigned)time(NULL));
 
   int n_in = 0;
@@ -12,6 +12,9 @@ int main() {
     double y = (double)rand() / RAND_MAX;
 
     if (x * x + y * y < 1e0) n_in += 1;
-    printf("%.10f\n", (double)n_in / iter);
+
+    // print every 1'000
+    if ((iter % 1'000) == 0) 
+      printf("%10d : %14.10f\n", iter, (double)n_in / iter * 4);
   }
 }
